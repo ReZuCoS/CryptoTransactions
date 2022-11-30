8d133db14f8ff2253ade8870cf8575e285f5267e
rezucos <tsyganok.vasya@yandex.ru>
2022-11-30 13:04:43 +0300

    Added patch entiti to update clients

---
+++ CryptoTransactions.API/Model/Entities/PatchEntities/ClientPatch.cs
@@ -0,0 +1,23 @@
+﻿namespace CryptoTransactions.API.Model.Entities
+{
+    /// <summary>
+    /// Client patch data
+    /// </summary>
+    public class ClientPatch
+    {
+        /// <summary>
+        /// Client's surname
+        /// </summary>
+        public string? Surname { get; set; }
+
+        /// <summary>
+        /// Client's name
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Client's patronymic (if available)
+        /// </summary>
+        public string? Patronymic { get; set; }
+    }
+}
