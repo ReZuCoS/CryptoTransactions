44ed140844c70a524a664bccc6751f85372ffb37
Василий Цыганок <tsyganok.vasya@yandex.ru>
2022-11-30 09:09:32 +0300

    Added database context

---
+++ CryptoTransactions.API/Model/CryptoTransactionsContext.cs
@@ -0,0 +1,37 @@
+﻿using CryptoTransactions.API.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoTransactions.API.Model
+{
+    public class CryptoTransactionsContext : DbContext
+    {
+        private static string? _connectionString;
+        public static string? ConnectionString
+        {
+            get => _connectionString;
+            set => _connectionString = !string.IsNullOrEmpty(value) ?
+                value : throw new ArgumentNullException(nameof(value));
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite(_connectionString);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasMany(c => c.SentTransactions)
+                .WithOne(t => t.Sender)
+                .HasForeignKey(t => t.SenderWallet)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Client>()
+                .HasMany(c => c.ReceivedTransactions)
+                .WithOne(t => t.Recipient)
+                .HasForeignKey(t => t.RecipientWallet)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
