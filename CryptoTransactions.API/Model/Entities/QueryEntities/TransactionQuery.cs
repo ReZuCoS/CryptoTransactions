4926ef54781efb7a752f300151fabdaf2a3dd33c
rezucos <tsyganok.vasya@yandex.ru>
2022-11-30 16:05:46 +0300

    Added transactionQuery class to make filter and patch for transactions

---
+++ CryptoTransactions.API/Model/Entities/QueryEntities/TransactionQuery.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Linq;
+
+namespace CryptoTransactions.API.Model.Entities
+{
+    /// <summary>
+    /// System transactions
+    /// </summary>
+    public class TransactionQuery
+    {
+        /// <summary>
+        /// Transaction date and time
+        /// </summary>
+        public string TimeStamp { get; set; } = string.Empty;
+
+        /// <summary>
+        /// GUID sender wallet number
+        /// </summary>
+        public string SenderWallet { get; set; } = string.Empty;
+
+        /// <summary>
+        /// GUID recipient wallet number
+        /// </summary>
+        public string RecipientWallet { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Currency type
+        /// </summary>
+        public string CurrencyType { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Transaction type
+        /// </summary>
+        public string TransactionType { get; set; } = string.Empty;
+
+        public bool IsEmpty()
+        {
+            return string.IsNullOrEmpty(TimeStamp) &&
+                string.IsNullOrEmpty(SenderWallet) &&
+                string.IsNullOrEmpty(RecipientWallet) &&
+                string.IsNullOrEmpty(CurrencyType) &&
+                string.IsNullOrEmpty(TransactionType);
+        }
+    }
+}
