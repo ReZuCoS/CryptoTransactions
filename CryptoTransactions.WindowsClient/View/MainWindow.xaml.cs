ff8aa7a1abbace56c63843e9ad414fac1154de2d
rezucos <tsyganok.vasya@yandex.ru>
2022-12-01 17:23:15 +0300

    Initialized WPF client project

---
+++ CryptoTransactions.WindowsClient/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CryptoTransactions.WindowsClient.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
