cb58b618ca9fdaadc96fe91aae9a92764e840864
rezucos <tsyganok.vasya@yandex.ru>
2022-12-08 14:24:49 +0300

    Added main window

---
+++ CryptoTransactions.WinClient/View/Windows/WindowMain.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CryptoTransactions.WinClient.View.Windows
+{
+    public partial class WindowMain : Window
+    {
+        public WindowMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
