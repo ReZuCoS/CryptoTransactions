2aa90b6b92c28f035f987fea5181595d21e321b0
rezucos <tsyganok.vasya@yandex.ru>
2022-12-09 15:46:16 +0300

    Added application model

---
+++ CryptoTransactions.WinClient/Model/Client.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoTransactions.WinClient.Model
+{
+    public class Client
+    {
+        public Client()
+        {
+            SentTransactions = new HashSet<Transaction>();
+            ReceivedTransactions = new HashSet<Transaction>();
+        }
+
+        public string WalletNumber { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public double Balance { get; set; }
+        public virtual ICollection<Transaction> SentTransactions { get; set; }
+        public virtual ICollection<Transaction> ReceivedTransactions { get; set; }
+        internal virtual ICollection<Transaction> Transactions =>
+            SentTransactions.Union(ReceivedTransactions)
+            .OrderBy(t => t.TimeStamp)
+            .ToList();
+    }
+}
---
+++ CryptoTransactions.WinClient/Model/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace CryptoTransactions.WinClient.Model
+{
+    public class Transaction
+    {
+        public Transaction() { }
+        public string GUID { get; set; }
+        public string TimeStamp { get; set; }
+        public string SenderWallet { get; set; }
+        public string RecipientWallet { get; set; }
+        public double Amount { get; set; }
+        public string CurrencyType { get; set; }
+        public string TransactionType { get; set; }
+        public virtual Client Sender { get; set; }
+        public virtual Client Recipient { get; set; }
+    }
+}
