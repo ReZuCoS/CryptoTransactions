9520c53759b87aa8b1345a4644d6e6c400cba814
rezucos <tsyganok.vasya@yandex.ru>
2022-12-12 11:21:10 +0300

    Added  entity list pages

---
+++ CryptoTransactions.WinClient/View/Pages/PageClientsList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CryptoTransactions.WinClient.Model;
+using CryptoTransactions.WinClient.Model.Entities;
+using System;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoTransactions.WinClient.View.Pages
+{
+    public partial class PageClientsList : Page
+    {
+        public PageClientsList()
+        {
+            InitializeComponent();
+
+            try
+            {
+                LoadClientsList();
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Ошибка при подключению к серверу!",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При выполнении программы произошла ошибка:\n" + ex.ToString(),
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void LoadClientsList()
+        {
+            listViewMain.ItemsSource = await WebApi.GetAll<Client>("/api/clients");
+        }
+    }
+}
---
+++ CryptoTransactions.WinClient/View/Pages/PageTransactionssList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CryptoTransactions.WinClient.Model;
+using CryptoTransactions.WinClient.Model.Entities;
+using System;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoTransactions.WinClient.View.Pages
+{
+    public partial class PageTransactionsList : Page
+    {
+        public PageTransactionsList()
+        {
+            InitializeComponent();
+
+            try
+            {
+                LoadTransactionsList();
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Ошибка при подключению к серверу!",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При выполнении программы произошла ошибка:\n" + ex.ToString(),
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void LoadTransactionsList()
+        {
+            listViewMain.ItemsSource = await WebApi.GetAll<Transaction>("/api/transactions");
+        }
+    }
+}
