5b07028f009decfebcc16913b06efa0083a995e6
Василий Цыганок <tsyganok.vasya@yandex.ru>
2022-12-12 07:43:34 +0300

    TODO: Extract pages from mainWindow

--- CryptoTransactions.WinClient/View/Windows/WindowMain.xaml.cs
+++ CryptoTransactions.WinClient/View/Windows/WindowMain.xaml.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using CryptoTransactions.WinClient.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
 using Newtonsoft.Json;
-using CryptoTransactions.WinClient.Model.Entities;
 
 namespace CryptoTransactions.WinClient.View.Windows
 {
     public partial class WindowMain : Window
     {
         IEnumerable<Client> _clients;
+        IEnumerable<Transaction> _transactions;
 
         private static readonly HttpClient _httpClient = new()
         {
@@ -22,6 +23,7 @@ namespace CryptoTransactions.WinClient.View.Windows
         {
             InitializeComponent();
             _clients = new List<Client>();
+            _transactions = new List<Transaction>();
         }
 
         private async void LoadClientsTable(object sender, RoutedEventArgs e)
@@ -52,6 +54,41 @@ namespace CryptoTransactions.WinClient.View.Windows
                 .Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<List<Client>>(content);
+        }
+
+        private async void LoadTransactionsTable(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _transactions = await GetTransactions();
+                listViewMain.ItemsSource = _transactions;
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Ошибка при подключению к серверу!",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При выполнении программы произошла ошибка:\n" + ex.ToString(),
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private static async Task<IEnumerable<Transaction>> GetTransactions()
+        {
+            var response = await _httpClient.GetAsync("/api/transactions");
+
+            string content = await response
+                .EnsureSuccessStatusCode()
+                .Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<List<Transaction>>(content);
+        }
+
+        private void listViewMain_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
         }
     }
 }
