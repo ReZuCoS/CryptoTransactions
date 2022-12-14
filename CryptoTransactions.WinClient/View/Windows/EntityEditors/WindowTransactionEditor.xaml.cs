9d9ae0fdd4b64a70163919a754c26953e5e4a4ae
rezucos <tsyganok.vasya@yandex.ru>
2022-12-14 16:17:47 +0300

    Added transaction editor window

---
+++ CryptoTransactions.WinClient/View/Windows/EntityEditors/WindowTransactionEditor.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CryptoTransactions.WinClient.Model.Entities;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoTransactions.WinClient.View.Windows.EntityEditors
+{
+    public partial class WindowTransactionEditor : Window
+    {
+        private readonly Transaction _transaction;
+
+        public WindowTransactionEditor()
+        {
+            InitializeComponent();
+            _transaction = new Transaction();
+            LoadTransactionData(_transaction);
+        }
+
+        public WindowTransactionEditor(Client sender) : this()
+        {
+            _transaction = new Transaction(sender);
+        }
+
+        private void LoadTransactionData(Transaction transaction)
+        {
+            DisableFieldIfPresented(txtBoxSenderWallet, transaction.SenderWallet);
+            DisableFieldIfPresented(txtBoxRecipientWallet, transaction.RecipientWallet);
+        }
+
+        private static void DisableFieldIfPresented(TextBox textBox, string? value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                textBox.Text = value;
+                textBox.IsEnabled = false;
+            }
+        }
+    }
+}
